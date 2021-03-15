9e49fcbc67658346bcc9f8f7354cff8213dc8ae8
Sangil <handal0131@gmail.com>
2021-03-15 14:01:26 +0900

    data preparing

--- app/data/prepare.py
+++ app/data/prepare.py
@@ -1,25 +1,28 @@
+import os
 import structlog
+import pandas as pd
 from tensorflow import keras
+
 
 
 logger = structlog.get_logger()
 
 
-def prepare_data(target_dataset):
+def prepare_data(config):
     default_list = [
         'fashion_mnist'
     ]
 
-    logger.info(f" - Prepare dataset [ {target_dataset} ]")
+    logger.info(f" - Prepare dataset [ {config['DATASET']} ]")
 
-    if target_dataset in default_list:
-        load_default_dataset(target_dataset)
+    if config['DATASET'] in default_list:
+        dataset = load_default_dataset(config['DATASET'])
     else:
-        logger.warn("load non-default dataset still not developed")
+        dataset = load_custom_dataset(config)
 
 
-def load_default_dataset(target_dataset):
-    logger.info(f" - '{target_dataset}' is now loading...")
+def load_default_dataset(config):
+    logger.info(f" - '{config['DATASET']}' is now loading...")
     load_dataset = {
         "fashion_mnist" : load_fashion_mnist
     }[target_dataset]
@@ -27,6 +30,45 @@ def load_default_dataset(target_dataset):
     (train_data, train_labels), (test_data, test_labels) = load_dataset()
     logger.info(f" - '{target_dataset}' is loaded")
     return (train_data, train_labels), (test_data, test_labels)
+
+
+def load_custom_dataset(config):
+    logger.info(f" - '{config['DATASET']}' is now loading...")
+
+    if config['DATASET_TYPE'] == 'CSV':
+        data_path = {
+            'train': os.path.join(config['DATASET_PATH'], 'train.csv'),
+            'valid': os.path.join(config['DATASET_PATH'], 'valid.csv'),
+            'test' : os.path.join(config['DATASET_PATH'], 'test.csv'),
+            'output': os.path.join(config['DATASET_PATH'], 'output.csv')
+        }
+
+        test_data = pd.read_csv(data_path['test'])
+        if os.path.exists(data_path['valid']):
+            train_data = pd.read_csv(data_path['test'])
+            valid_data = pd.read_csv(data_path['test'])
+        else:
+            train_data, valid_data = train_valid_split(config, data_path)
+
+    logger.info(
+        f" - '{config['DATASET']}' is loaded \n"
+        f" Train {len(train_data)} rows, "
+        f" Valid {len(valid_data)} rows, "
+        f" Test  {len(test_data)} rows"
+    )
+    return train_data, valid_data, test_data
+    
+
+def train_valid_split(config, data_path):
+    logger.info(f" - splitting valid set split_rate [{config['SPLIT_RATE']}]")
+    
+    train = pd.read_csv(data_path['train'])
+    split_idx = int(len(train) * config['SPLIT_RATE'])
+    train_data = train[:split_idx]
+    valid_data = train[split_idx:]
+
+    return train_data, valid_data
+
 
 
 def load_fashion_mnist():
--- app/run.py
+++ app/run.py
@@ -1,3 +1,4 @@
+import os
 import structlog
 from app.exc.exc import HANException
 from app.data.prepare import prepare_data
@@ -8,31 +9,36 @@ logger = structlog.get_logger()
 
 def run():
     # TODO: Load config
-    target_dataset = "fashion_mnist"
-    
+    config = {
+        'DATASET': 'pubg-finish-placement-prediction',
+        'DATASET_PATH': os.path.join('D:', 'datasets', 'pubg-finish-placement-prediction'),
+        'DATASET_TYPE': 'CSV',
+        'SPLIT_RATE': 0.8
+    }
+
     try:
-       logger.info("Step 1 >> Data Preparation")
-       prepare_data(target_dataset)
-       logger.info(" - 1.1 : Data Collection ")
-       logger.info(" - 1.2 : Data Cleaning")
-       logger.info(" - 1.3 : Data Augmentation")
+        logger.info("Step 1 >> Data Preparation")
+        logger.info(" - 1.1 : Data Collection ")
+        prepare_data(config)
 
+        logger.info(" - 1.2 : Data Cleaning")
+        logger.info(" - 1.3 : Data Augmentation")
 
-       logger.info("Step 2 >> Feature Engineering")
+        logger.info("Step 2 >> Feature Engineering")
 
-       logger.info(" - 2.1 : Feature Selection")
-       logger.info(" - 2.2 : Feature Extraction")
-       logger.info(" - 2.3 : Feature Construction")
+        logger.info(" - 2.1 : Feature Selection")
+        logger.info(" - 2.2 : Feature Extraction")
+        logger.info(" - 2.3 : Feature Construction")
+ 
+        logger.info("Step 3 >> Model Generation")
+        logger.info(" - 3.1 : Neural Architecture")
+        logger.info(" - 3.2 : Architecture optimization")
 
-       logger.info("Step 3 >> Model Generation")
-       logger.info(" - 3.1 : Neural Architecture")
-       logger.info(" - 3.2 : Architecture optimization")
+        logger.info("Step 4 >> Model Setting")
 
-       logger.info("Step 4 >> Model Setting")
+        logger.info("Step 5 >> Model Evaluation")
 
-       logger.info("Step 5 >> Model Evaluation")
-
-       logger.info("Step 6 >> Output")
+        logger.info("Step 6 >> Output")
 
     except HANException as e:
         logger.warn(f"Custom Exception raised : {e.message}")
