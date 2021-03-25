4b0d3fda9d7358d680f49a3b7759ca3637e8a484
Sangil <handal0131@gmail.com>
2021-03-25 17:48:31 +0900

    add check skew and kurtois

--- app/data/check.py
+++ app/data/check.py
@@ -1,76 +1,68 @@
 import structlog
-import numpy as np
 import pandas as pd
+import numpy as np
 
 logger = structlog.get_logger()
-
-
-def check_null_values(config, dataset):
-    logger.info(f" - Check Null value data...")
-
-    train_null = get_null_values(dataset['train'], 'train')
-    test_null = get_null_values(dataset['test'], 'test')
-
-    null_display = pd.merge(train_null, test_null, how='outer', on='Features')
-    logger.info(f"\n{null_display}")
-
-    return dataset
-
-
-def get_null_values(dataset, name):
-    if dataset is None:
-        return None
-
-    na_info_data = []
-    for col in dataset.columns:
-        na_count = dataset[col].isna().sum()
-        if na_count > 0:
-            na_percent = np.round((100 * (na_count)/len(dataset)), 2)            
-            na_info ={
-                'Features' : col,
-                f'NA_{name} (count)': na_count,
-                f'NA_{name} (%)': na_percent
-            }
-            na_info_data.append(na_info)
-
-    data_frame = pd.DataFrame(na_info_data, index=None).sort_values(by=f'NA_{name} (count)', ascending=False)
-
-    return data_frame
 
 
 def check_cardinal_values(config, dataset):
     logger.info(f" - Check Cardinal value data...")
     THRESH = config['CARDINAL_THRESHOLD']
 
-    cat_features = list()
-    num_features = list()
-    for col in dataset['train'].columns:
-        if (dataset['train'][col].dtype == 'object') and (col != config['TARGET_LABEL']):
-            cat_features.append(col)
-        else:
-            num_features.append(col)
+    cat_features = dataset.select_dtypes(include='object').drop(columns=["_SET_"])
+    num_features = dataset.select_dtypes(exclude='object')
 
-    low_card_cols = list()
-    mid_card_cols = list()
-    high_card_cols = list()
-
+    card_info_data = []
     for col in cat_features:
-        col_unique = dataset['train'][col].nunique()
+        col_unique = dataset[col].nunique()
+
+        category = None
         if col_unique < THRESH[0]:
-            low_card_cols.append(col)
+            category = 'low'
         elif col_unique < THRESH[1]:
-            mid_card_cols.append(col)
+            category = 'mid'
         else:
-            high_card_cols.append(col)
-
-    # dataset['train'] = dataset['train'].drop(cat_features, axis=1)
-    # dataset['test'] = dataset['test'].drop(cat_features, axis=1)
+            category = 'high'
         
-    logger.info(
-        f"\n low cardinal cols(<{THRESH[0]}) #{len(low_card_cols)}\n {low_card_cols}"
-        f"\n mid cardinal cols(<{THRESH[1]}) #{len(mid_card_cols)}\n {mid_card_cols}"
-        f"\nhigh cardinal cols(>={THRESH[1]}) #{len(high_card_cols)}\n {high_card_cols}"
-    )
+        card_info = {
+            'Features': col,
+            'nunique': col_unique,
+            'category': category
+        }
+        card_info_data.append(card_info)
 
-    print(dataset['train'].head(10).T)
+    data_frame = pd.DataFrame(card_info_data, index=None).sort_values(by=f'nunique', ascending=False)
+    logger.info(f"\n{data_frame}")
+
+    return dataset
+
+
+def check_skewness_kurtosis(config, dataset):
+    logger.info(f" - Check Numeric value data...")
+
+    num_features = dataset.select_dtypes(exclude='object')
+    log_dataset = np.log1p(num_features)
+    sqrt_dataset = np.sqrt(num_features)
+    log_sqrt_dataset = np.log1p(np.sqrt(num_features))
+    
+    info_data = []
+    for col in num_features:
+        info = {
+            'Features' : col,
+
+            'Skew': f"{dataset[col].skew():5.2f}",
+            'S(L)': f"{log_dataset[col].skew():5.2f}",
+            'S(S)': f"{sqrt_dataset[col].skew():5.2f}",
+            'S(LS)': f"{log_sqrt_dataset[col].skew():5.2f}",
+
+            'Kurt': f"{dataset[col].kurt():5.2f}",
+            'K(L)': f"{log_dataset[col].kurt():5.2f}",
+            'K(S)': f"{sqrt_dataset[col].kurt():5.2f}",
+            'K(LS)': f"{log_sqrt_dataset[col].kurt():5.2f}",
+        }
+        info_data.append(info)
+
+    data_frame = pd.DataFrame(info_data, index=None).sort_values(by=f'Features', ascending=False)
+    logger.info(f"\n{data_frame}")
+
     return dataset
--- app/run.py
+++ app/run.py
@@ -2,10 +2,11 @@ import os
 import structlog
 from app.exc.exc import HANException
 from app.data.prepare import prepare_data
-from app.data.clean import clean_empty_label, clean_duplicate
-from app.data.check import check_null_values, check_cardinal_values
+from app.data.clean import clean_empty_label, clean_duplicate, clean_null_column
+from app.data.check import check_cardinal_values, check_skewness_kurtosis
 from app.feature.split import split_label_feature, split_train_valid, shuffle_train_data
 from app.feature.select import select_feature
+from app.feature.encoding import one_hot_encoding
 from app.feature.scale import scale_feature
 from app.model.model import build_model, fit_model, estimate_model
 
@@ -24,7 +25,8 @@ def run():
         'TARGET_LABEL': 'SalePrice',
         'CLEAN_EMPTY_LABEL': True,
         'CLEAN_DUPLICATE': True,
-        'CARDINAL_THRESHOLD': [6, 9],
+        'CLEAN_NULL_THRESHOLD': 0.5,
+        'CARDINAL_THRESHOLD': [5, 9],
         'SPLIT_RATE': 0.8,
         'DATA_SHUFFLE': True,
         'MODEL': 'XGBoost'
@@ -38,23 +40,27 @@ def run():
         logger.info(" - 1.2 : Data Cleaning")
         dataset = clean_duplicate(config, dataset)
         dataset = clean_empty_label(config, dataset)
+        dataset = clean_null_column(config, dataset)
 
         logger.info(" - 1.3 : Data Check")
-        dataset = check_null_values(config, dataset)
         dataset = check_cardinal_values(config, dataset)
-
+        dataset = check_skewness_kurtosis(config, dataset)
         logger.info(" - 1.3 : Data Augmentation")
 
         logger.info("Step 2 >> Feature Engineering")
         logger.info(" - 2.1 : Feature Selection")
         dataset = select_feature(config, dataset)
+        dataset = one_hot_encoding(config, dataset)
+        # dataset = check_cardinal_values(config, dataset)
 
         logger.info(" - 2.2 : Feature Extraction")
         logger.info(" - 2.3 : Feature Construction")
         logger.info(" - 2.4 : Feature Scaling")
-        dataset = shuffle_train_data(config, dataset)
+
         dataset = split_label_feature(config, dataset)
-        train, test = scale_feature(config, dataset)
+        # dataset = scale_feature(config, dataset)
+
+        # dataset = shuffle_train_data(config, dataset)
 
  
         logger.info("Step 3 >> Model Generation")
@@ -65,13 +71,13 @@ def run():
 
         logger.info("Step 4 >> Model Setting")
         logger.info(" - 4.0 : Data Split")
-        train, valid = split_train_valid(config, train)
+        train, valid = split_train_valid(config, dataset)
 
         logger.info("Step 5 >> Model Evaluation")
         model = fit_model(model, train)
-        estimate_model(model)
 
         logger.info("Step 6 >> Output")
+        estimate_model(model)
 
     except HANException as e:
         logger.warn(f"Custom Exception raised : {e.message}")
