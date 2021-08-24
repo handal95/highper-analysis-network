9cc4ed668e4b34ba0f32b99e8b2174dc835bedf5
handal <handal__@naver.com>
2021-08-25 03:29:12 +0900

    band

--- timeseries/utils/visualize.py
+++ timeseries/utils/visualize.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import time
 
 
 class Dashboard:
@@ -11,13 +12,17 @@ class Dashboard:
 
         self.fig, self.ax = self.init_figure()
         self.data = None
+        self.pred = None
+        self.upper = None
+        self.lower = None
+
         self.preds = self.initialize()
-        self.pred0 = None
-        self.pred1 = None
-        self.pred2 = None
-        self.pred3 = None
-        self.pred4 = None
-        self.pred5 = None
+
+        self.band_flag = False
+        self.upper_band = list()
+        self.lower_band = list()
+        
+        self.area_up1 = None
         self.area_upper = None
         self.area_lower = None
         self.labels = self.initialize()
@@ -56,6 +61,84 @@ class Dashboard:
         
         return value
 
+    def data_concat(self, target, x, cond):
+        if target is None:
+            return x[:cond]
+        
+        return np.concatenate((target[:-cond], x[:cond + 1]))
+
+    def pred_concat(self, target, y, cond):
+        if target is None:
+            return y
+        
+        length = len(target) - self.sequence + cond
+        return np.concatenate((target[:length + 2], y[cond + 1:]))
+    
+    def visualize(self, x, y, cond, normalize=True):
+        if normalize:
+            x = self.dataset.denormalize(x)
+            y = self.dataset.denormalize(y)
+
+        x = x[0].detach().numpy().ravel()
+        y = y[0].detach().numpy().ravel()
+        
+        self.data = self.data_concat(self.data, x, cond)
+        self.pred = self.pred_concat(self.pred, y, cond)
+
+        if self.band_flag is False:
+            self.area_up1 = y[:cond]
+            self.area_up2 = y[:cond]
+            self.area_up3 = y[:cond]
+            self.area_down1 = y[:cond]
+            self.area_down2 = y[:cond]
+            self.area_down3 = y[:cond]
+            self.band_flag = True
+            
+            self.upper = y[:cond]
+            self.lower = y[:cond]
+
+        std = y.std()
+        median = np.median(y[cond:])
+
+        self.upper = np.append(self.upper, y[cond:].max())
+        self.lower = np.append(self.lower, y[cond:].min())
+
+        self.area_up1 = np.append(self.area_up1, median + 1 * std)
+        self.area_up2 = np.append(self.area_up2, median + 2 * std)
+        self.area_up3 = np.append(self.area_up3, median + 3 * std)
+        self.area_down1 = np.append(self.area_down1, median - 1 * std)
+        self.area_down2 = np.append(self.area_down2, median - 2 * std)
+        self.area_down3 = np.append(self.area_down3, median - 3 * std)
+
+        fig, ax = self.fig, self.ax
+        try:
+            ax.clear()
+            ax.grid()
+            
+            length = len(self.pred) - cond
+            plt.axvspan(length - self.sequence, length - self.sequence + cond, facecolor='green', alpha=0.01)
+            plt.axvspan(length - self.sequence + cond, length, facecolor='gray', alpha=0.5)
+
+            length = np.arange(len(self.area_up1[cond:]))
+            ax.fill_between(length, self.area_down3[cond:], self.area_up3[cond:], color='red', alpha=0.1)
+            ax.fill_between(length, self.area_down2[cond:], self.area_up2[cond:], color='blue', alpha=0.2)
+            ax.fill_between(length, self.area_down1[cond:], self.area_up1[cond:], color='blue', alpha=0.3)
+            ax.plot(self.data[cond:], "r-", linewidth=2, alpha=0.6, label="data")
+            ax.plot(self.pred[cond:], "b-", linewidth=3, alpha=0.2, label="pred")
+
+
+            xtick = np.arange(0, self.scope * 2, 12)
+            values = self.time[0:self.scope * 2:12]
+
+            plt.ylim(self.dataset.min, self.dataset.max)
+            plt.xticks(xtick, values, rotation=30)
+
+            fig.show()
+            fig.canvas.draw()
+            fig.canvas.flush_events()
+        except (KeyboardInterrupt, AttributeError) as e:
+            raise
+        
 
     def _visualize(self, time, data, pred):
         self.idx += 1
@@ -68,42 +151,41 @@ class Dashboard:
             def concat(target, x, loc=None):
                 if target is None:
                     if loc is None:
-                        target = x[:3]
+                        target = x[:6]
                     else:
                         target = x[:loc]
                 else:
                     if loc is None:
-                        target = np.concatenate((target, x[3:4]))
+                        target = np.concatenate((target, x[6:7]))
                     else:
                         target = np.concatenate((target, x[loc:loc+1]))
                 return target
             
             self.data = concat(self.data, data)
-            min_scope = max(0, self.data.size - self.scope + 1)
+            min_scope = max(self.sequence, self.data.size - self.scope + 1)
             if self.area_lower is None:
-                self.area_lower = pred[:4]
-                self.area_upper = pred[:4]
+                self.area_lower = pred[:6]
+                self.area_upper = pred[:6]
+
             min_axvln = min(0, self.area_lower.size)
 
-            self.area_upper = np.append(self.area_upper, pred[4:].max())
-            self.area_lower = np.append(self.area_lower, pred[4:].min())
+            self.area_upper = np.append(self.area_upper, pred[6:].max())
+            self.area_lower = np.append(self.area_lower, pred[6:].min())
 
-            self.pred2 = concat(self.pred2, pred, 3)
-            self.pred3 = concat(self.pred3, pred, 6)
-            self.pred4 = concat(self.pred4, pred, 9)
-            self.pred5 = concat(self.pred5, pred, 11)
-            # self._time = concat(self._time, time)
             ax.clear()
             ax.grid()
             
             for i in range(self.sequence):
-                self.labels[i] = concat(self.labels[i], self.label[self.idx, i])
+                self.labels[i] = concat(self.labels[i], self.label[self.idx, i], loc=0)
                 self.preds[i] = np.append(self.preds[i], pred[i])
+            
+            plt.axvspan(10, 40, facecolor='gray')
 
             # print(self.dataset.label[min_scope:min_scope+self.scope, :, 0])
-            for i in range(self.sequence):
-                # ax.plot(self.labels[i][min_scope:], alpha=0.4, linewidth=(12-i), label=f"Label {i}")
-                ax.plot(self.preds[i][min_scope:], alpha=0.4, linewidth=i//2, label=f"Preds {i}")
+            for i in range(7, self.sequence):
+                # ax.plot(self.labels[i][min_scope:], alpha=0.4, linewidth=i//2, label=f"Label {i}")
+                # print(self.labels[i][min_scope:])
+                ax.plot(self.preds[i][min_scope:], alpha=0.4, linewidth=1, label=f"Preds {i}")
                 
                 
             # print(f"Pred length : {len(self.pred5)}")
@@ -117,14 +199,15 @@ class Dashboard:
             ax.plot(self.area_lower[min_scope:], "b-", linewidth=2, alpha=0.6, label="Lower")
             ax.legend()
             
-            xtick = np.arange(0, self.scope, 12)
             # ax.fill_between(np.arange(10), self.area_lower[min_scope:], self.area_upper[min_scope:], color='lightgray', alpha=0.5)
             
+            xtick = np.arange(0, self.scope, 12)
             values = self.time[min_scope:min_scope + self.scope:12]
             plt.xticks(xtick, values, rotation=30)
 
-            # ax.relim()
-            # ax.autoscale_view()
+            plt.ylim(self.dataset.min, self.dataset.max)
+            ax.relim()
+            fig.legend()
 
             fig.show()
             fig.canvas.draw()
@@ -146,7 +229,6 @@ def plt_loss(gen_loss, dis_loss, path, num):
         path + "/Loss_" + str(num) + ".png", bbox_inches="tight", pad_inches=0.5
     )
     plt.close()
-
 
 def plt_progress(real, fake, epoch, path):
     real = np.squeeze(real)
